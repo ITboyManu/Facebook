2622bc7075ab09c76ac3a205864fd9349a4d3161
Manish_Pc <mk>
2023-08-06 22:03:34 +0530

    commit

--- app/src/main/java/com/example/facebook/Signup.kt
+++ app/src/main/java/com/example/facebook/Signup.kt
@@ -3,20 +3,22 @@ package com.example.facebook
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 import com.example.facebook.databinding.ActivityMainBinding
 
 class Signup : AppCompatActivity() {
-    lateinit var binding: ActivityMainBinding
+    lateinit var text:TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding= ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+
+        setContentView(R.layout.activity_signup)
         supportActionBar?.hide()
 
+        text=findViewById<TextView>(R.id.txt)
 
-
-
-
-
+        text.setOnClickListener {
+            var intent= Intent(this,MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
